3a28771115cc005323c55f943d9735637b52e517
Dossanbayev Roma <rahmandosanbayev@gmail.com>
2026-06-04 13:58:35 +0500

    i changed a little

--- ToDo/ContentView.swift
+++ ToDo/ContentView.swift
@@ -9,23 +9,69 @@ import SwiftUI
 
 
 
+struct TaskItem: Identifiable {
+    let id = UUID()
+    var isCompleted: Bool = false
+    var title: String
+}
+
+
+
 
 struct ContentView: View {
     
+    @State private var tasks: [TaskItem] = [
+            TaskItem(title: "Купить продукты"),
+            TaskItem(title: "Учить Swift"),
+            TaskItem(title: "Сделать проект"),
+            TaskItem(title: "Сделать зарядку")
+        ]
     
-    @State private var tasks = ["Купить продукты", "Учить Swift", "Сделать проект"]
     
     
+    @State private var newTask = ""
+
     var body: some View {
         
-        NavigationView {
-                List(tasks, id: \.self) { task in
-                    Text(task)
+        
+        VStack {
+            
+            NavigationView {
+                
+                List(tasks) { task in HStack {
+                    
+                    
+                    Button(action: {
+                        if let index = tasks.firstIndex(where: { $0.id == task.id }) {
+                            tasks[index].isCompleted.toggle()
+                        }
+                    }) {
+                    Image(systemName: task.isCompleted ? "checkmark.circle.fill" : "circle")
+                    .foregroundColor(task.isCompleted ? .green : .gray)
+                    .font(.title3)
+                                            }
                 }
-                .navigationTitle("To Do List")
+                    
+                }
             }
-
+            
+        }
+        
+        HStack {
+                        TextField("Новая задача", text: $newTask)
+                            .textFieldStyle(RoundedBorderTextFieldStyle())
+                        
+                        Button("Добавить") {
+                            // Проверяем, что строка не пустая
+                            if !newTask.isEmpty {
+                                let item = TaskItem(title: newTask)
+                                tasks.append(item)
+                                newTask = ""
+                            }
+                        }
+                    }
         .padding()
+        .navigationTitle("To Do List")
     }
 }
 
