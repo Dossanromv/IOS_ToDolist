4f145ef185b8f4b3461be6921790a7333bc83f89
Dossanbayev Roma <rahmandosanbayev@gmail.com>
2026-06-08 18:59:15 +0500

    I changed everything

--- ToDo/ContentView.swift
+++ ToDo/ContentView.swift
@@ -20,68 +20,34 @@ struct TaskItem: Identifiable {
 
 struct ContentView: View {
     
-    @State private var tasks: [TaskItem] = [
-            TaskItem(title: "Купить продукты"),
-            TaskItem(title: "Учить Swift"),
-            TaskItem(title: "Сделать проект"),
-            TaskItem(title: "Сделать зарядку")
-        ]
-    
-
-    
+    @State private var tasks = ["Купить продукты", "Учить Swift", "Сделать проект"]
     @State private var newTask = ""
+
 
     var body: some View {
         
-        
-        VStack {
-            
-            NavigationView {
-                
-                
-                List(tasks, id: \.self) { task in
-                            Text(task)
-                        }
+        NavigationView {
+            VStack {
 
+                List(tasks, id: \.self) { task in
+                    Text(task)
+                }
+                HStack {
+                    TextField("Новая задача", text: $newTask)
+                    Button("Добавить") {
+                        tasks.append(newTask)
+                        newTask = ""
+                    } 
+                }
                 
-                List(tasks) { task in HStack {
-                    
-                    
-                    
-                    Button(action: {
-                        if let index = tasks.firstIndex(where: { $0.id == task.id }) {
-                            tasks[index].isCompleted.toggle()
-                        }
-                    }) {
-                    Image(systemName: task.isCompleted ? "checkmark.circle.fill" : "circle")
-                    .foregroundColor(task.isCompleted ? .green : .gray)
-                    .font(.title3)
-                                            }
-                }
-                    
-                }
-                .navigationTitle("To Do List")
+                
             }
+            .navigationTitle("To Do List")
             
         }
-        
-        HStack {
-                        TextField("Новая задача", text: $newTask)
-                            .textFieldStyle(RoundedBorderTextFieldStyle())
-                        
-                        Button("Добавить") {
-                            // Проверяем, что строка не пустая
-                            if !newTask.isEmpty {
-                                let item = TaskItem(title: newTask)
-                                tasks.append(item)
-                                newTask = ""
-                            }
-                        }
-                    }
-        .padding()
-        .navigationTitle("To Do List")
     }
 }
+
 
 #Preview {
     ContentView()
